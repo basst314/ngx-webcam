8aebcaa25539febae698ea3301af8e454767575e
Sebastian Schlote <sebastian.schlote@gmail.com>
2018-01-14 01:02:51 -0800

    Added Trigger for Webcam-Component

--- src/app/modules/webcam/webcam/webcam.component.ts
+++ src/app/modules/webcam/webcam/webcam.component.ts
@@ -1,23 +1,31 @@
-import {AfterViewInit, Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, Input, OnDestroy, Output, ViewChild} from '@angular/core';
 import {WebcamInitError} from "../domain/webcam-init-error";
 import {WebcamImage} from "../domain/webcam-image";
+import {Observable} from "rxjs/Observable";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'webcam',
   templateUrl: './webcam.component.html',
   styleUrls: ['./webcam.component.scss']
 })
-export class WebcamComponent implements AfterViewInit {
+export class WebcamComponent implements AfterViewInit, OnDestroy {
 
   /** Defines the max width of the webcam area in px */
   @Input() public width: number = 640;
   /** Defines the max height of the webcam area in px */
   @Input() public height: number = 480;
+  /** If this Observable emits, an image will be captured and emitted through 'imageCapture' EventEmitter */
+  private _trigger: Observable<void>;
+  private triggerSubscription: Subscription;
 
   /** EventEmitter which fires when an image has been captured */
   @Output() public imageCapture: EventEmitter<WebcamImage> = new EventEmitter<WebcamImage>();
   /** Emits a mediaError if webcam cannot be initialized (e.g. missing user permissions) */
   @Output() public initError: EventEmitter<WebcamInitError> = new EventEmitter<WebcamInitError>();
+
+  /** Emits when the webcam video was clicked */
+  @Output() public imageClick: EventEmitter<void> = new EventEmitter<void>();
 
   @ViewChild('video') private video: any;
   // Canvas for Video Snapshots
@@ -25,21 +33,27 @@ export class WebcamComponent implements AfterViewInit {
 
 
   public ngAfterViewInit(): void {
-    let _video = this.video.nativeElement;
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      // TODO allow video options as Input()
-      navigator.mediaDevices.getUserMedia({video: {facingMode: "environment"}})
-        .then(stream => {
-          _video.srcObject = stream;
-          _video.play();
-        })
-        .catch((err: MediaStreamError) => {
-          this.initError.next(<WebcamInitError>{message: err.message, mediaStreamError: err});
-          console.warn("Error initializing webcam", err);
-        });
-    } else {
-      this.initError.next(<WebcamInitError> {message: "Cannot read UserMedia from MediaDevices."});
+    this.initWebcam();
+  }
+
+  public ngOnDestroy(): void {
+    this.unsubscribeFromSubscriptions();
+  }
+
+  /**
+   * If the given Observable emits, an image will be captured and emitted through 'imageCapture' EventEmitter
+   */
+  @Input()
+  public set trigger(trigger: Observable<void>) {
+    if (this.triggerSubscription) {
+      this.triggerSubscription.unsubscribe();
     }
+    this._trigger = trigger;
+
+    // Subscribe on events from this Observable to take snapshots
+    this.triggerSubscription = this._trigger.subscribe(() => {
+      this.takeSnapshot();
+    });
   }
 
   /**
@@ -69,4 +83,33 @@ export class WebcamComponent implements AfterViewInit {
     this.imageCapture.next(new WebcamImage(dataUrl, mimeType));
   }
 
+  /**
+   * Init webcam live view
+   */
+  private initWebcam() {
+    let _video = this.video.nativeElement;
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      // TODO allow video options as Input()
+      navigator.mediaDevices.getUserMedia({video: {facingMode: "environment"}})
+        .then(stream => {
+          _video.srcObject = stream;
+          _video.play();
+        })
+        .catch((err: MediaStreamError) => {
+          this.initError.next(<WebcamInitError>{message: err.message, mediaStreamError: err});
+          console.warn("Error initializing webcam", err);
+        });
+    } else {
+      this.initError.next(<WebcamInitError> {message: "Cannot read UserMedia from MediaDevices."});
+    }
+  }
+
+  /**
+   * Unsubscribe from all open subscriptions
+   */
+  private unsubscribeFromSubscriptions() {
+    if (this.triggerSubscription) {
+      this.triggerSubscription.unsubscribe();
+    }
+  }
 }
