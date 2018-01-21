8a5fc3776a9fd9d7b0ea5efc6f2f366be5f617a6
Sebastian Schlote <sebastian.schlote@gmail.com>
2018-01-21 14:06:15 -0800

    add webcam-component to public api

--- public_api.ts
+++ public_api.ts
@@ -1,3 +1,4 @@
 export * from './src/app/modules/webcam/webcam.module';
+export * from './src/app/modules/webcam/webcam/webcam.component';
 export * from './src/app/modules/webcam/domain/webcam-image';
 export * from './src/app/modules/webcam/domain/webcam-init-error';
