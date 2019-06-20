c66c7e0868453ba0714a3e6a72ae831d64080b6e
David Renner <david@fam-renner.de>
2019-06-20 23:35:58 +0200

    cleared dependencies and prepared packagr

--- public_api.ts
+++ public_api.ts
@@ -1,6 +1,7 @@
 export * from './src/app/modules/webcam/webcam.module';
 export * from './src/app/modules/webcam/webcam/webcam.component';
 export * from './src/app/modules/webcam/domain/webcam-image';
+export * from './src/app/modules/webcam/domain/webcam-video';
 export * from './src/app/modules/webcam/domain/webcam-init-error';
 export * from './src/app/modules/webcam/domain/webcam-mirror-properties';
 export * from './src/app/modules/webcam/util/webcam.util';
