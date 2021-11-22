d414a63ca3c069f85b3ca52c768184e511f02a0a
yamaha252 <mail@yamaha252.ru>
2021-11-22 11:04:11 +0500

    test teardown to the default state

--- src/test.ts
+++ src/test.ts
@@ -9,9 +9,7 @@ declare const require: any;
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(), {
-    teardown: { destroyAfterEach: false }
-}
+  platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
